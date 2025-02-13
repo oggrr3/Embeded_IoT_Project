4833c2742206e94827a597abe9bac11e3d5ad45a
oggr3 <caovi797@gmail.com>
2025-02-13 17:11:10 +0700

    adjust server_ble

--- server_BLE.py
+++ server_BLE.py
@@ -1,5 +1,6 @@
 # server.py
 
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -25,4 +26,6 @@ def send_data():
     return jsonify(latest_data), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
